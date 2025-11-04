e2e1c79cee1311b00be53159d548c059f040a46f
ihorhnennyi <ihorhnennyi@gmail.com>
2025-11-04 14:50:04 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,40 +2,29 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': '/src',
-			'@assets': '/src/assets',
-			'@components': '/src/components',
-			'@config': '/src/config',
-			'@hooks': '/src/hooks',
-			'@layouts': '/src/layouts',
-			'@pages': '/src/pages',
-			'@router': '/src/router',
-			'@store': '/src/store',
-			'@styles': '/src/styles',
-			'@types': '/src/types',
-			'@utils': '/src/utils',
-		},
-	},
-	base: '/',
-	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:8080',
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
-	preview: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:8080',
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@config': '/src/config',
+      '@hooks': '/src/hooks',
+      '@layouts': '/src/layouts',
+      '@pages': '/src/pages',
+      '@router': '/src/router',
+      '@store': '/src/store',
+      '@styles': '/src/styles',
+      '@types': '/src/types',
+      '@utils': '/src/utils'
+    },
+    dedupe: ['react', 'react-dom']
+  },
+  base: '/',
+  plugins: [react()],
+  server: {
+    proxy: { '/api': { target: 'http://localhost:8080', changeOrigin: true, secure: false } }
+  },
+  preview: {
+    proxy: { '/api': { target: 'http://localhost:8080', changeOrigin: true, secure: false } }
+  }
 })
